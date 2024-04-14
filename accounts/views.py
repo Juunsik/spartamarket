b915d76838613ec0405bda6915613b56e590277c
Juunsik <Juunsik29@gmail.com>
2024-04-14 19:11:12 +0900

    Add: accounts
    - login, logout, signup, update, delete, password, profile urls.py, views.py 기본 틀
    - templates 생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def delete(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def change_password(request):
+    pass
+
+
+def profile(request, username):
+    pass
+
