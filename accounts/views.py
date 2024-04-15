c1465f5a6395c1a241250d1aaf3a730172ceda5e
Juunsik <Juunsik29@gmail.com>
2024-04-15 12:50:49 +0900

    Add: logout
    - views.py/logout 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_POST
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -24,7 +24,7 @@ def login(request):
         form=AuthenticationForm(request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            next_url=request.GET.get('next') or 'index'
+            next_url=request.GET.get('next') or 'index' # 메인 페이지: 추후 생성 예정
             return redirect(next_url)
     else:
         form=AuthenticationForm()
@@ -32,8 +32,11 @@ def login(request):
     return render('request', 'accounts/login.html', context)
 
 
+@require_POST
 def logout(request):
-    pass
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('index') # 메인 페이지: 추후 생성 예정
 
 
 def delete(request):
