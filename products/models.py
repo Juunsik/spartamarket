40bb299bdc19197586dcac74616f7a4b87b9cbf4
Juunsik <Juunsik29@gmail.com>
2024-04-15 17:27:27 +0900

    Add: products models.py
    - products model 구성

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Products(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=300)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
+    wish=models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='wish')
+    hits=models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='hits')
+    image=models.ImageField(upload_to='images/', blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
