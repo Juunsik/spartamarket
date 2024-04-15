6066cc7ea806c2a6f55d93d597720d4a0a50b1e4
Juunsik <Juunsik29@gmail.com>
2024-04-15 23:21:32 +0900

    Add: product delete
    - product_delete 추가

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=300)
     author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
--- products/views.py
+++ products/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Products
+from django.views.decorators.http import require_POST
+from .models import Product
 from .forms import ProductForm
 
 # Create your views here.
 def product_list(request):
-    products=Products.objects.all()
+    products=Product.objects.all()
     context={'products':products}
     return render(request, 'products/product_list.html', context)
 
@@ -26,17 +27,36 @@ def product_create(request):
 
 
 def product_detail(request, pk):
-    product=get_object_or_404(Products, pk=pk)
+    product=get_object_or_404(Product, pk=pk)
     context={'product':product}
     return render(request, 'products/product_detail.html', context)
 
-
+@login_required
 def product_update(request, pk):
-    pass
+    product=get_object_or_404(Product, pk=pk)
+    if product.author==request.user:
+        if request.method=='POST':
+            form=ProductForm(request.POST, instance=product)
+            if form.is_valid():
+                product=form.save()
+                return redirect('products:detail', product.pk)
+        else:
+            form=ProductForm(instance=product)
+            context={
+                'form':form,
+                'product':product,
+                }
+            return render(request, 'products/product_update.html', context)
+    else:
+        return redirect('products:list') 
 
-
+@login_required
 def product_delete(request, pk):
-    pass
+    product=get_object_or_404(Product, pk=pk)
+    if request.user.is_authenticated:
+        if product.author==request.user:
+            product.delete()
+    return redirect('products:list')
 
 
 def product_wish(request, pk):
