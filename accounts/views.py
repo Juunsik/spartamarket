eeb223dd8f1b0315133704ee86f5f0070a53078b
Juunsik <Juunsik29@gmail.com>
2024-04-15 16:02:47 +0900

    Add: profile
    - 데이터를 넘기는 views만 구현
    - 나머지는 product 구현 이후 예정

--- accounts/views.py
+++ accounts/views.py
@@ -1,42 +1,48 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import (
+    login as auth_login,
+    logout as auth_logout,
+    get_user_model,
+)
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.http import require_http_methods, require_POST
 from .forms import CustomUserCreationForm
 
-# Create your views here.
-@require_http_methods(['GET','POST'])
-def signup(request):
-    if request.method=='POST':
-        form=CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user=form.save()
-            auth_login(request, user)
-            return redirect('profile')
-    else:
-        form=CustomUserCreationForm()
-    context={'form':form}
-    return render(request, 'accounts/signup.html', context)        
 
-@require_http_methods(['GET','POST'])
+# Create your views here.
+@require_http_methods(["GET", "POST"])
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("profile")
+    else:
+        form = CustomUserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
+
+@require_http_methods(["GET", "POST"])
 def login(request):
-    if request.method=='POST':
-        form=AuthenticationForm(request.POST)
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            next_url=request.GET.get('next') or 'index' # 메인 페이지: 추후 생성 예정
+            next_url = request.GET.get("next") or "index"  # 메인 페이지: 추후 생성 예정
             return redirect(next_url)
     else:
-        form=AuthenticationForm()
-    context={'form':form}
-    return render('request', 'accounts/login.html', context)
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
 
 
 @require_POST
 def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
-    return redirect('index') # 메인 페이지: 추후 생성 예정
+    return redirect("index")  # 메인 페이지: 추후 생성 예정
 
 
 def delete(request):
@@ -52,5 +58,8 @@ def change_password(request):
 
 
 def profile(request, username):
-    pass
-
+    member = get_object_or_404(get_user_model(), username=username)
+    context = {
+        "memeber": member,
+    }
+    return render(request, "accounts/profile.html", context)
