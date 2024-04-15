97dc3a817cc761b33260594bf52867663039224c
Juunsik <Juunsik29@gmail.com>
2024-04-15 12:44:18 +0900

    Add: login
    - views.py/login 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_http_methods
 from .forms import CustomUserCreationForm
 
 # Create your views here.
+@require_http_methods(['GET','POST'])
 def signup(request):
     if request.method=='POST':
         form=CustomUserCreationForm(request.POST)
@@ -15,9 +18,18 @@ def signup(request):
     context={'form':form}
     return render(request, 'accounts/signup.html', context)        
 
-
+@require_http_methods(['GET','POST'])
 def login(request):
-    pass
+    if request.method=='POST':
+        form=AuthenticationForm(request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_url=request.GET.get('next') or 'index'
+            return redirect(next_url)
+    else:
+        form=AuthenticationForm()
+    context={'form':form}
+    return render('request', 'accounts/login.html', context)
 
 
 def logout(request):
