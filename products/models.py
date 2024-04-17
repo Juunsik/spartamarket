bf09d710c4a29801d281b64576deaf08f8d9730b
Juunsik <Juunsik29@gmail.com>
2024-04-17 21:05:20 +0900

    Update: accounts/views.py, urls.py, profile.html
    - follow 기능 추가

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=300)
     author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
-    wish=models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='wish', blank=True)
+    wish=models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='wishs', blank=True)
     hits=models.PositiveIntegerField(default=0)
     image=models.ImageField(upload_to='images/')
     created_at=models.DateTimeField(auto_now_add=True)
