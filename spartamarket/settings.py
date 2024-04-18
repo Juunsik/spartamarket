921610015af5e1d793ee7feca228fb68126e017d
Juunsik <Juunsik29@gmail.com>
2024-04-18 22:47:22 +0900

    Update: product_list, detail css update
    - 상품 목록(메인)페이지, 상세 페이지 css 구현

--- spartamarket/settings.py
+++ spartamarket/settings.py
@@ -42,6 +42,7 @@ SYSTEM_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.humanize',
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
