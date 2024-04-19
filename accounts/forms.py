01cd0991da10201c7719938fe5d61ba53ae1d17e
Juunsik <Juunsik29@gmail.com>
2024-04-19 10:09:20 +0900

    Update: login, profile, signup.html css
    - login.html css 작업
    - signup.html css 작업, placeholder
    - profile.html css 작업, 이미지 처리

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ClearableFileInput, TextInput, PasswordInput, CharField
 from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model=get_user_model()
-        fields=UserCreationForm.Meta.fields+()
+        fields=UserCreationForm.Meta.fields+('image',)
         
+        widgets={
+            'username': TextInput(attrs={'placeholder':'username', 'class':"form-control form-control-lg"}),
+            'image': ClearableFileInput(attrs={'class':'form-control'}),
+        }
         
+    password1=CharField(widget=PasswordInput(attrs={'placeholder':'password', 'class':'form-control'}))
+    password2=CharField(widget=PasswordInput(attrs={'placeholder':'password confirm', 'class':'form-control'}))
